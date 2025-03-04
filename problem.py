c82a991ba6998522103143117f88302b3f51f536
XiYoZu <139477511+XiYoZu@users.noreply.github.com>
2025-03-04 10:34:37 +0000

    Add files via upload

--- problem.py
+++ problem.py
@@ -1,1 +1,20 @@
+import numpy as np
+
+
+sample = 1000
+
+number = 90
+choice = []
+count = 0
+for i in range(sample):
+    step = 1
+    while step < number:
+        step += int(np.random.choice([1,2]))
+        if step == number:
+            count += 1
+    
+prob = count/sample
+print(prob)
+
+
 
